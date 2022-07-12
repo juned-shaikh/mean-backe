93e31acb6974007824f9e86fb5496935aca8295f
unknown <you@example.com>
2022-07-12 18:31:18 +0530

    m

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const cors = require('cors');
 
 const mongoose = require('./db.js');
 const routes = require('./routes/routes.js')
-
+const port = process.env.PORT || 8000;
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors({origin:'http://localhost:4200'}));
 
-app.listen(3000, ()=>console.log('server started at port 3000'))
+app.listen(port, ()=>console.log('server started at port 3000'))
 
 
 app.use('/employees',routes)
